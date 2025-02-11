a25e0b88b6cc2965082474cd644c6390a42d95cb
l-martinetti <luca.martinetti@outlook.it>
2025-02-11 17:41:54 +0100

    feat: add MovieDetails basic layout

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,17 @@
 
 const MovieDetails = () => {
     return (
-        <div>MovieDetails</div>
+        <section className="container my-3">
+            <div className="row border-bottom">
+                <div className="col col-4">
+                    <p>movie img</p>
+                </div>
+                <div className="col col-8">
+                    <h2>Movie title</h2>
+                    <p>Movie description</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
